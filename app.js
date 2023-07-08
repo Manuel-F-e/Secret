d365a95b9b34288905a88002bcf06a1763f420b3
Manuel-F-e <lx00635@innovaccion.mx>
2023-07-08 01:15:16 -0600

    mod sex

--- app.js
+++ app.js
@@ -8,11 +8,26 @@ let btnSi = document.getElementById("btn_si");
 let btnNo = document.getElementById("btn_no");
 let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
 
+
 btnNo.addEventListener('mouseenter', function(e) { moverPosicionRandom(e.target) })
 
 btnSi.addEventListener('click', function(e) {
     alert('Te llevaré a la Luna para mirar las estrellas y aún así seguiría viendo tus preciosos ojos ')
+    
     divModoSexo.style.display = 'block';
     const cancion = new Audio('img\\milky-chance-letra-espanol-ingles.mp3')
+
     cancion.play();
+});
+
+// divModoSexo.addEventListener('click', function(e) {
+   // divModoSexo.innerHTML = "<img src=https://i.pinimg.com/474x/1a/23/43/1a23431f1a5b6fb2b3f9e15bfa0b028d.jpg >"
+// })
+
+botones = document.getElementsByTagName('button')
+console.log(botones)
+
+window.addEventListener('beforeunload', (event) => {
+    event.preventDefault();
+    event.returnValue = "";
 });
